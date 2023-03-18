07fc0fac527cad6d8b20b317c21ba684c6d520d6
alex-s4 <al39726867@gmail.com>
2023-03-18 13:38:09 +0800

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+package com.alexproject.firstproject;
+
+public class HelloWorld {
+	public String greet() {
+		return "Hello World";
+	}
+}
---
+++ HelloWorldTest.java
@@ -0,0 +1,11 @@
+package com.alexproject.firstproject;
+
+public class HelloWorldTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HelloWorld helloworld = new HelloWorld();
+		System.out.println(helloworld.greet());
+	}
+
+}
